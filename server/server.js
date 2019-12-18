e1057f8cc401849647f46b3270fa5007cfbe871e
Natalia Malesa <nmalesa@gmail.com>
2019-12-18 12:28:35 -0600

    Write test get request on server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const db = require('../db/index.js');
+const {pool, asyncFunction} = require('../db/index.js');
 
 app.use(cors());
 app.use('/', express.static('public'));
@@ -10,15 +10,25 @@ app.use('/', express.static('public'));
 app.use('/bundle', express.static('public/bundle.js'));
 app.use('/styleSheet', express.static('public/styles.css'));
 
-app.get(`/:id`, function (req, res) {
-    db.getImage(req.params.id, (error, result) => {
-        if (error) {
-            res.send(error);
-        } else {
-            res.send(result);
-        }
-    })
+app.get('/test', (req, res) => {
+  asyncFunction((error, results) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(results);
+    }
+  });
 });
+
+// app.get(`/:id`, function (req, res) {
+//     db.getImage(req.params.id, (error, result) => {
+//         if (error) {
+//             res.send(error);
+//         } else {
+//             res.send(result);
+//         }
+//     })
+// });
 
 app.listen(3010, function() {
     console.log('listening')
