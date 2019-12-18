511770448650670c27c4fa5fe559e804343043f3
Natalia Malesa <nmalesa@gmail.com>
2019-12-18 12:21:44 -0600

    Set up security considertions with dotenv

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM node:10
-COPY package*.json ./
-RUN npm ci --only=production
-COPY . .
-EXPOSE 3010
-CMD ["node", "server/server.js"]
--- db/index.js
+++ db/index.js
@@ -1,28 +1,31 @@
 const mariadb = require('mariadb');
+require('dotenv').config()
 
 const pool = mariadb.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   connectionLimit: 5
 });
 
 async function asyncFunction() {
   let conn;
   try {
-    conn = await pool.getConnection();
-    const rows = await conn.query("SELECT 1 as val");
-    // rows: [ { val: 1}, meta: ... ]
+      conn = await pool.getConnection();
+      const rows = await conn.query("SELECT 1 as val");
+      console.log(rows); // rows: [ { val: 1}, meta: ... ]
 
-    const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
-    // res: { affectedRows: 1, insertId: 1, warningStatus: 0 }
+      const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+      console.log(res); // res: { affectedRows: 1, insertId: 1, warningStatus: 0 }
   } catch (err) {
-    throw err;
+      throw err;
   } finally {
-    if (conn) conn.release(); // release to pool
+    // if (conn) conn.release(); // release to pool
+      if (conn) return conn.end();
   }
 }
 
-
+module.exports = {pool, asyncFunction};
 
 // const mysqlConfig = require('./config.js');
 //
