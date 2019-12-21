0f17356f257c7d9d280ac9d4c934664deeb4f5ff
Natalia Malesa <nmalesa@gmail.com>
2019-12-21 14:08:27 -0600

    Update schema to include join table

--- db/index.js
+++ db/index.js
@@ -3,19 +3,6 @@ const faker = require('faker');
 const _ = require('lodash');
 require('dotenv').config();
 
-
-
-
-
-const thumbnails = {
-  product_id: _.random(1, 10000000),
-  thumb_1: faker.image.imageUrl(),  // Or null (randomize)
-  thumb_2: faker.image.imageUrl(),
-  thumb_3: faker.image.imageUrl(),
-  thumb_4: faker.image.imageUrl(),
-  thumb_5: faker.image.imageUrl()
-};
-
 const pool = mariadb.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -43,8 +30,21 @@ async function addImage(name, primary_image, video_embed, description) {
   let connection;
   try {
     connection = await pool.getConnection();
-    let postRequest = await connection.query(`INSERT INTO products (name, primary_image, video_embed, description) VALUES (?, ?, ?, ?)`, [name, primary_image, video_embed, description]);
-    return postRequest;
+    let postImageRequest = await connection.query(`INSERT INTO products (name, primary_image, video_embed, description) VALUES (?, ?, ?, ?)`, [name, primary_image, video_embed, description]);
+    return postImageRequest;
+  } catch (error) {
+    throw error;
+  } finally {
+    if (connection) connection.release();
+  }
+};
+
+async function addThumbnails(product_id, thumb_1, thumb_2, thumb_3, thumb_4, thumb_5) {
+  let connection;
+  try {
+    connection = await pool.getConnection();
+    let postThumbRequest = await connection.query(`INSERT INTO thumbnails (product_id, thumb_1, thumb_2, thumb_3, thumb_4, thumb_5) VALUES (?, ?, ?, ?, ?, ?)`, [product_id, thumb_1, thumb_2, thumb_3, thumb_4, thumb_5]);
+    return postThumbRequest;
   } catch (error) {
     throw error;
   } finally {
@@ -99,4 +99,4 @@ async function deleteImage(id) {
 //   }
 // }
 
-module.exports = {pool, retrieveImage, addImage, modifyImage, deleteImage};
+module.exports = {pool, retrieveImage, addImage, addThumbnails, modifyImage, deleteImage};
--- server/server.js
+++ server/server.js
@@ -4,43 +4,11 @@ const faker = require('faker');
 const _ = require('lodash');
 const app = express();
 const port = process.env.PORT || 3010;
-const {pool, retrieveImage, addImage, modifyImage, deleteImage} = require('../db/index.js');
+const {pool, retrieveImage, addImage, addThumbnails, modifyImage, deleteImage} = require('../db/index.js');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-// const video_id = faker.random.alphaNumeric(11);
-//
-// const seedProduct = () => {
-//
-// };
-//
-// const product = {
-//   id: 1, // Increment by 1
-//   name: faker.commerce.productName(),
-//   primary_image: faker.image.imageUrl(),
-//   video_embed: `https://www.youtube.com/watch?v=${video_id}`,
-//   description: faker.company.catchPhrase()
-// };
-//
-// const thumbnails = {
-//   product_id: _.random(1, 10000000),
-//   thumb_1: faker.image.imageUrl(),  // Or null (randomize)
-//   thumb_2: faker.image.imageUrl(),
-//   thumb_3: faker.image.imageUrl(),
-//   thumb_4: faker.image.imageUrl(),
-//   thumb_5: faker.image.imageUrl()
-// };
-//
-// app.get('/test', async (req, res) => {
-//   try {
-//     let test1 = await console.log(product);
-//     res.send(test1);
-//   } catch (error) {
-//     res.send(error);
-//   }
-// });
 
 const video_id = faker.random.alphaNumeric(11);
 
@@ -51,6 +19,16 @@ const product = {
   description: faker.company.catchPhrase()
 };
 
+const thumbnails = {
+  product_id: _.random(1, 10000000),
+  thumb_1: faker.image.imageUrl(),  // Or null (randomize)
+  thumb_2: faker.image.imageUrl(),
+  thumb_3: faker.image.imageUrl(),
+  thumb_4: faker.image.imageUrl(),
+  thumb_5: faker.image.imageUrl()
+};
+
+// CRUD OPERATIONS - GET
 app.get('/products/:id', async (req, res) => {
   try {
     let retrievedImage = await retrieveImage(req.params.id);
@@ -60,6 +38,7 @@ app.get('/products/:id', async (req, res) => {
   }
 });
 
+// CRUD OPERATIONS - POST
 app.post('/products', async (req, res) => {
   try {
     let newImage = await addImage(product.name, product.primary_image, product.video_embed, product.description);
@@ -69,6 +48,16 @@ app.post('/products', async (req, res) => {
   }
 });
 
+app.post('/thumbnails', async (req, res) => {
+  try {
+    let newThumbs = await addThumbnails(thumbnails.product_id, thumbnails.thumb_1, thumbnails.thumb_2, thumbnails.thumb_3, thumbnails.thumb_4, thumbnails.thumb_5);
+    res.send(newThumbs);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+// CRUD OPERATIONS - PUT
 app.put('/products/:id', async (req, res) => {
   try {
     let modifiedImage = await modifyImage(req.params.id);
@@ -78,6 +67,7 @@ app.put('/products/:id', async (req, res) => {
   }
 });
 
+// CRUD OPERATIONS - POST
 app.delete('/products/:id', async (req, res) => {
   try {
     let deletedImage = await deleteImage(req.params.id);
