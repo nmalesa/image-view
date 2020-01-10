d87a8e31f73d7d3cadfd976944f3ea4cb4e11c4a
Natalia Malesa <nmalesa@gmail.com>
2020-01-10 12:11:09 -0600

    Test sequelize connection

--- sequelize/index.js
+++ sequelize/index.js
@@ -1,5 +1,7 @@
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
+// Passing parameters separately.  May also pass a connection URI.
 const sequelize = new Sequelize('images', process.env.DB_user, process.env.DB_pass, {
   host: process.env.DB_HOST,
   dialect: 'mariadb'
@@ -8,6 +10,17 @@ const sequelize = new Sequelize('images', process.env.DB_user, process.env.DB_pa
   pool: {
     max: 5,
     min: 0,
+    acquire: 30000,
     idle: 10000
   }
 });
+
+// Testing the connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(() => {
+    console.error('Unable to connect to the database:', err);
+  });
