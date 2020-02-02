0e91373aadcaf74368a12d075bbcebc99196237c
Natalia Malesa <nmalesa@gmail.com>
2020-02-02 14:32:47 -0600

    Remove unnecessary dependencies

--- mariadb/db.js
+++ mariadb/db.js
@@ -26,61 +26,61 @@ async function retrieveImage(id) {
 };
 
 // CRUD OPERATIONS - POST
-async function addImage(name, primary_image, video_embed, description) {
-  let connection;
-  try {
-    connection = await pool.getConnection();
-    let postImageRequest = await connection.query(`INSERT INTO products (name, primary_image, video_embed, description) VALUES (?, ?, ?, ?)`, [name, primary_image, video_embed, description]);
-    return postImageRequest;
-  } catch (error) {
-    throw error;
-  } finally {
-    if (connection) connection.release();
-  }
-};
+// async function addImage(name, primary_image, video_embed, description) {
+//   let connection;
+//   try {
+//     connection = await pool.getConnection();
+//     let postImageRequest = await connection.query(`INSERT INTO products (name, primary_image, video_embed, description) VALUES (?, ?, ?, ?)`, [name, primary_image, video_embed, description]);
+//     return postImageRequest;
+//   } catch (error) {
+//     throw error;
+//   } finally {
+//     if (connection) connection.release();
+//   }
+// };
 
-async function addThumbnails(product_id, thumb_1, thumb_2, thumb_3, thumb_4, thumb_5) {
-  let connection;
-  console.log(product_id);
-  try {
-    connection = await pool.getConnection();
-    let postThumbRequest = await connection.query('INSERT INTO thumbnails (product_id, thumb_1, thumb_2, thumb_3, thumb_4, thumb_5) VALUES (?, ?, ?, ?, ?, ?)', [product_id, thumb_1, thumb_2, thumb_3, thumb_4, thumb_5]);
-    return postThumbRequest;
-  } catch (error) {
-    throw error;
-  } finally {
-    if (connection) connection.release();
-  }
-};
+// async function addThumbnails(product_id, thumb_1, thumb_2, thumb_3, thumb_4, thumb_5) {
+//   let connection;
+//   console.log(product_id);
+//   try {
+//     connection = await pool.getConnection();
+//     let postThumbRequest = await connection.query('INSERT INTO thumbnails (product_id, thumb_1, thumb_2, thumb_3, thumb_4, thumb_5) VALUES (?, ?, ?, ?, ?, ?)', [product_id, thumb_1, thumb_2, thumb_3, thumb_4, thumb_5]);
+//     return postThumbRequest;
+//   } catch (error) {
+//     throw error;
+//   } finally {
+//     if (connection) connection.release();
+//   }
+// };
 
 // CRUD OPERATIONS - PUT
-async function modifyImage(id) {
-  let connection;
-  try {
-    connection = await pool.getConnection();
-    let putRequest = await connection.query(`UPDATE products SET description="This is a new description" WHERE id=${id}`);
-    return putRequest;
-  } catch (error) {
-    throw error;
-  } finally {
-    if (connection) connection.release();
-  }
-};
+// async function modifyImage(id) {
+//   let connection;
+//   try {
+//     connection = await pool.getConnection();
+//     let putRequest = await connection.query(`UPDATE products SET description="This is a new description" WHERE id=${id}`);
+//     return putRequest;
+//   } catch (error) {
+//     throw error;
+//   } finally {
+//     if (connection) connection.release();
+//   }
+// };
 
 // CRUD OPERATIONS - DELETE
-async function deleteImage(id) {
-  let connection;
-  try {
-    connection = await pool.getConnection();
-    let deleteRequest = await connection.query(`DELETE FROM products WHERE id=${id}`);
-    console.log('test');
-    return deleteRequest;
-  } catch (error) {
-    throw error;
-  } finally {
-    if (connection) connection.release();
-  }
-};
+// async function deleteImage(id) {
+//   let connection;
+//   try {
+//     connection = await pool.getConnection();
+//     let deleteRequest = await connection.query(`DELETE FROM products WHERE id=${id}`);
+//     console.log('test');
+//     return deleteRequest;
+//   } catch (error) {
+//     throw error;
+//   } finally {
+//     if (connection) connection.release();
+//   }
+// };
 
 // EXAMPLE SET-UP FOR MARIADB NODE.JS DRIVER
 // async function asyncFunction() {
@@ -100,4 +100,4 @@ async function deleteImage(id) {
 //   }
 // }
 
-module.exports = {pool, retrieveImage, addImage, addThumbnails, modifyImage, deleteImage};
+module.exports = { pool, retrieveImage };
--- mariadb/index.js
+++ mariadb/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {pool, retrieveImage, addImage, addThumbnails, modifyImage, deleteImage} = require('./db.js');
+const {pool, retrieveImage } = require('./db.js');
 
 const app = express();
 const port = process.env.PORT || 3010;
@@ -20,42 +20,42 @@ app.get('/products/:id', async (req, res) => {
 });
 
 // CRUD OPERATIONS - POST
-app.post('/products', async (req, res) => {
-  try {
-    let newImage = await addImage(product.name, product.primary_image, product.video_embed, product.description);
-    res.send(newImage);
-  } catch (error) {
-    res.send(error);
-  }
-});
-
-app.post('/thumbnails', async (req, res) => {
-  try {
-    let newThumbs = await addThumbnails(thumbnails.product_id, thumbnails.thumb_1, thumbnails.thumb_2, thumbnails.thumb_3, thumbnails.thumb_4, thumbnails.thumb_5);
-    res.send(newThumbs);
-  } catch (error) {
-    res.send(error);
-  }
-});
+// app.post('/products', async (req, res) => {
+//   try {
+//     let newImage = await addImage(product.name, product.primary_image, product.video_embed, product.description);
+//     res.send(newImage);
+//   } catch (error) {
+//     res.send(error);
+//   }
+// });
+//
+// app.post('/thumbnails', async (req, res) => {
+//   try {
+//     let newThumbs = await addThumbnails(thumbnails.product_id, thumbnails.thumb_1, thumbnails.thumb_2, thumbnails.thumb_3, thumbnails.thumb_4, thumbnails.thumb_5);
+//     res.send(newThumbs);
+//   } catch (error) {
+//     res.send(error);
+//   }
+// });
 
 // CRUD OPERATIONS - PUT
-app.put('/products/:id', async (req, res) => {
-  try {
-    let modifiedImage = await modifyImage(req.params.id);
-    res.send(modifiedImage);
-  } catch (error) {
-    res.send(error);
-  }
-});
+// app.put('/products/:id', async (req, res) => {
+//   try {
+//     let modifiedImage = await modifyImage(req.params.id);
+//     res.send(modifiedImage);
+//   } catch (error) {
+//     res.send(error);
+//   }
+// });
 
 // CRUD OPERATIONS - POST
-app.delete('/products/:id', async (req, res) => {
-  try {
-    let deletedImage = await deleteImage(req.params.id);
-    res.send(deletedImage);
-  } catch (error) {
-    res.send(error);
-  }
-});
+// app.delete('/products/:id', async (req, res) => {
+//   try {
+//     let deletedImage = await deleteImage(req.params.id);
+//     res.send(deletedImage);
+//   } catch (error) {
+//     res.send(error);
+//   }
+// });
 
 app.listen(port, console.log(`Listening on port ${port}...`));
