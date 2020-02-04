316b0bf220d08e9878d669c584d53d49bc905cab
Natalia Malesa <nmalesa@gmail.com>
2020-02-04 10:14:50 -0600

    Refactored MongoDB connection

--- mongodb/db.js
+++ mongodb/db.js
@@ -1,15 +1,37 @@
-const { MongoClient } = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
 const url = 'mongodb://localhost:27017';
 
 const dbName = 'images';
 
-let db;
+const client = new MongoClient(url, { useUnifiedTopology: true });
 
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-  if (err) return console.log(err);
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log('Connected successfully to server');
 
-  db = client.db(dbName);
-  console.log(`Connected to MongoDB: ${url}`);
-  console.log(`Database: ${dbName}`);
+  const db = client.db(dbName);
+
+  client.close();
 });
+
+
+
+// const { MongoClient } = require('mongodb');
+//
+// const url = 'mongodb://localhost:27017';
+//
+// const dbName = 'images';
+//
+// let db;
+//
+// MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+//   if (err) return console.log(err);
+//
+//   db = client.db(dbName);
+//   console.log(`Connected to MongoDB: ${url}`);
+//   console.log(`Database: ${dbName}`);
+//
+//   db.products.find({ name: 'Ergonomic Wooden Bacon' });
+// });
