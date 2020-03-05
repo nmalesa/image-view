c91f3e31b4c763df2c1a5ce6ce06ea7b4ba60347
Natalia Malesa <nmalesa@gmail.com>
2020-03-04 20:16:13 -0600

    Spin up servers for MongoDB and Mongoose

--- mongoose/db.js
+++ mongoose/db.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const faker = require('faker');
-const _ = require('lodash');
 
 mongoose.connect('mongodb://localhost:27017/images', {
   useNewUrlParser: true,
@@ -19,52 +17,3 @@ connection.once('open', function() {
     });
   });
 });
-
-const imageSchema = new mongoose.Schema({
-  name: String,
-  primaryImage: String,
-  videoEmbed: String,
-  description: String,
-  thumbnails: [ String ]
-});
-
-const Image = mongoose.model('Image', imageSchema);
-
-async function createImage() {
-  const image = new Image({
-    name: faker.commerce.productName(),
-    primaryImage: faker.image.image(),
-    videoEmbed: _.random(1, 10) <= 5 ? `https://www.youtube.com/watch?v=${faker.random.alphaNumeric(11)}` : null,
-    description: faker.company.catchPhrase(),
-    thumbnails: ['null', 'null', 'null', 'null', 'null']
-  });
-
-  image.thumbnails[0] = _.random(1, 10) <= 5 ? faker.image.image() : null;
-  image.thumbnails[1] = image.thumbnails[0] !== null ? (_.random(1, 10) <= 5 ? faker.image.image() : null) : null;
-  image.thumbnails[2] = image.thumbnails[1] !== null ? (_.random(1, 10) <= 5 ? faker.image.image() : null) : null;
-  image.thumbnails[3] = image.thumbnails[2] !== null ? (_.random(1, 10) <= 5 ? faker.image.image() : null) : null;
-  image.thumbnails[4] = image.thumbnails[3] !== null ? (_.random(1, 10) <= 5 ? faker.image.image() : null) : null;
-
-  const result = await image.save();
-
-  console.log(result);
-};
-
-// async function getImage(id) {
-//   const image = await Image.findById(id);
-//
-//   console.log(image);
-// };
-//
-// async function getImageByName() {
-//   const image = await Image
-//     .find({ name: 'Ergonomic Wooden Bacon' })
-//
-//   console.log(image);
-// }
-
-// createImage();
-
-// getImage('5e38b27e5887660933a1e011');
-
-// getImageByName();
