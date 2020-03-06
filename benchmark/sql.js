3ba041cf9ca0877c58e3d306183f804ef6962ca0
Natalia Malesa <nmalesa@gmail.com>
2020-03-06 12:35:49 -0600

    Set up connection to images database with MariaDB using promises for benchmarking

--- benchmark/sql.js
+++ benchmark/sql.js
@@ -2,76 +2,65 @@ const Benchmark = require('benchmark');
 const suite = new Benchmark.Suite;
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
-const ProductModel = require('../sql/sequelize/models/product.js');
-const ThumbnailModel = require('../sql/sequelize/models/thumbnail.js');
+// const Sequelize = require('sequelize');
+// const ProductModel = require('../sql/sequelize/models/product.js');
+// const ThumbnailModel = require('../sql/sequelize/models/thumbnail.js');
 
-const sequelize = new Sequelize('imageViews', 'root', 'password', {
-  host: 'localhost',
-  dialect: 'mariadb',
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-});
-
-// Promise.all([
-//   sequelize.authenticate()
-//     .then(() => {
-//       console.log('Connection has been established successfully.');
-//     })
-//     .catch(err => {
-//       console.error('Unable to connect to the database:', err);
-//     })
-// ]).then(() => {
-//   const Product = ProductModel(sequelize, Sequelize);
-//   const Thumbnail = ThumbnailModel(sequelize, Sequelize);
-//
-//   Thumbnail.belongsTo(Product);
-
-//   suite.add('Sequelize', () => {
-//     Product.findAll({
-//       where: {
-//         id: 8629947
-//       }
-//     })
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err))
-//   })
-//   .on('cycle', event => {
-//     console.log(String(event.target));
-//   })
-//   .run({ async: true })
+// const sequelize = new Sequelize('imageViews', 'root', 'password', {
+//   host: 'localhost',
+//   dialect: 'mariadb',
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
 // });
 
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  connectionLimit: 5,
+  database: 'images'
+});
+
+pool.getConnection()
+  .then(conn => {
+    return conn.query('SELECT * FROM products INNER JOIN thumbnails ON products.id = thumbnails.thumb_id WHERE products.id = 8629947');
+  })
+  .then(res => {
+    console.log(res);
   })
   .catch(err => {
-    console.error('Unable to connect to the database:', err);
+    console.log(err);
   })
 
-const Product = ProductModel(sequelize, Sequelize);
-const Thumbnail = ThumbnailModel(sequelize, Sequelize);
-
-Product.hasMany(Thumbnail);
-Thumbnail.belongsTo(Product);
-
-const query = () => {
-  Product.findAll({
-    where: {
-      id: 8629947
-    },
-    include: [{
-      model: Thumbnail,
-      required: true
-    }]
-  })
-  .then(data => console.log(data))
-  .catch(err => console.log(err))
-};
-
-query();
+// sequelize.authenticate()
+//   .then(() => {
+//     console.log('Connection has been established successfully.');
+//   })
+//   .catch(err => {
+//     console.error('Unable to connect to the database:', err);
+//   })
+//
+// const Product = ProductModel(sequelize, Sequelize);
+// const Thumbnail = ThumbnailModel(sequelize, Sequelize);
+//
+// Product.hasMany(Thumbnail);
+// Thumbnail.belongsTo(Product);
+//
+// const query = () => {
+//   Product.findAll({
+//     where: {
+//       id: 8629947
+//     },
+//     include: [{
+//       model: Thumbnail,
+//       required: true
+//     }]
+//   })
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err))
+// };
