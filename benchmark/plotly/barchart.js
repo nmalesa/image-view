0f8c1feeb29297bf69afeb5f2c757dd1b21cbdfd
Natalia Malesa <nmalesa@gmail.com>
2020-03-09 15:51:18 -0500

    Create bar chart structure

--- benchmark/plotly/barchart.js
+++ benchmark/plotly/barchart.js
@@ -1,1 +1,31 @@
-const Plotly = require('plotly.js-dist');
+const nosql = {
+  x: ['NoSQL', 'SQL'],
+  y: [3, 6],
+  name: 'ORM',
+  error_y: {
+    type: 'data',
+    array: [1, 0.5],
+    visible: true
+  },
+  type: 'bar'
+};
+
+const sql = {
+  x: ['NoSQL', 'SQL'],
+  y: [4, 7],
+  name: 'Without ORM',
+  error_y: {
+    type: 'data',
+    array: [0.5, 2],
+    visible: true
+  },
+  type: 'bar'
+};
+
+const data = [nosql, sql];
+
+const layout = { barmode: 'group' };
+
+console.log('Hello world!');
+
+Plotly.newPlot('benchmark', data, layout);
