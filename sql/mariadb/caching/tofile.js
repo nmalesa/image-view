64045d175fffaf1130323477d0fa00299d482883
Natalia Malesa <nmalesa@gmail.com>
2020-03-18 10:40:28 -0500

    Create cache to file middleware

---
+++ sql/mariadb/caching/tofile.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { pool, retrieveImage } = require('../db.js');
+const flatCache = require('flat-cache');
+
+const app = express();
+const port = process.env.PORT || 3030;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let cache = flatCache.load('productsCache');
+
+let flatCacheMiddleware = (req, res, next) => {
+  let key = '__express__' + req.originalUrl || req.url;
+  let cacheContent = cache.getKey(key);
+
+  if (cacheContent) {
+    res.send(cacheContent);
+  } else {
+    res.sendResponse = res.send;
+    res.send = body => {
+      cache.setKey(key, body);
+      cache.save();
+      res.sendResponse(body);
+    }
+    next();
+  }
+};
+
+app.get('/products/:id', async (req, res) => {
+  try {
+    let retrievedImage = await retrieveImage(req.params.id);
+    res.send(retrievedImage);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.listen(port, console.log(`Listening on port ${port}...`));
