314a1238de3571f399df0e43987eda720d7164bd
Natalia Malesa <nmalesa@gmail.com>
2020-03-18 16:45:19 -0500

    Create Redis middleware that caches successfully using Postman

---
+++ sql/mariadb/caching/redis.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { pool, retrieveImage } = require('../db.js');
+const redis = require('redis');
+
+const app = express();
+const port = process.env.PORT || 3030;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let client = redis.createClient();
+
+let redisMiddleware = (req, res, next) => {
+  let key = '__express__' + req.originalUrl || req.url;
+
+  client.get(key, (err, reply) => {
+    if (reply) {
+      res.send(reply);
+    } else {
+      res.sendResponse = res.send;
+      res.send = body => {
+        client.set(key, JSON.stringify(body));
+        res.sendResponse(body);
+      }
+      next();
+    }
+  });
+};
+
+app.get('/products/:id', redisMiddleware, async (req, res) => {
+  try {
+    let retrievedImage = await retrieveImage(req.params.id);
+    res.send(retrievedImage);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.listen(port, console.log(`Listening on port ${port}...`));
