392116eea7c9a42efce487f5f8303ae519b8ed32
Natalia Malesa <nmalesa@gmail.com>
2020-03-19 18:08:47 -0500

    Refactor load test to include a check and target a single endpoint

--- load-testing/load-test.js
+++ load-testing/load-test.js
@@ -1,5 +1,5 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, check } from 'k6';
 
 export let options = {
   stages: [
@@ -19,32 +19,9 @@ export let options = {
 const BASE_URL = 'http://localhost:3030/products';
 
 export default () => {
-  let responses = http.batch([
-    [
-      'GET',
-      `${BASE_URL}/6384294/`,
-      null,
-      { tags: { ctype: 'html', name: 'Practical Steel Computer' } }
-    ],
-    [
-      'GET',
-      `${BASE_URL}/7049283/`,
-      null,
-      { tags: { ctype: 'html', name: 'Intelligent Plastic Bike' } }
-    ],
-    [
-      'GET',
-      `${BASE_URL}/8629947/`,
-      null,
-      { tags: { ctype: 'html', name: 'Gorgeous Concrete Sausages' } }
-    ],
-    [
-      'GET',
-      `${BASE_URL}/9858273`,
-      null,
-      { tags: { ctype: 'html', name: 'Generic Plastic Fish' } }
-    ]
-  ]);
+  let image = http.get(`${BASE_URL}/8629958/`).json();
+
+  check(image, { 'retrieved image': (obj) => obj.length > 0 });
 
   sleep(1);
 };
