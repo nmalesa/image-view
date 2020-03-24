931f888f9616b9c7bb477f61b74610b0614edc18
Natalia Malesa <nmalesa@gmail.com>
2020-03-23 19:30:17 -0500

    Check transaction time for MongoDB server via smoke test

--- load-testing/smoke-test.js
+++ load-testing/smoke-test.js
@@ -15,10 +15,12 @@ const BASE_URL = 'http://localhost:3030/products';
 export default () => {
   let image = http.get(`${BASE_URL}/5e0980925349f02e58d1c27b/`);
 
+  // Add .json() to end of image to use this check:
   // check(image, { 'retrieved image': (obj) => obj.length > 0 });
 
   check(image, {
-    'status was 200': r => r.status === 200
+    'status was 200': r => r.status === 200,
+    'transaction time OK': r => r.timings.duration < 200
   });
 
   sleep(1);
