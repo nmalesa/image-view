353cbc9dc2021221a298861a83a110b6bcdc6dd7
Natalia Malesa <nmalesa@gmail.com>
2020-03-28 13:57:59 -0500

    Reconfigure production server to test Docker-Nginx

--- docker-nginx/backend/src/index.js
+++ docker-nginx/backend/src/index.js
@@ -3,37 +3,42 @@ const { pool, retrieveImage } = require('/Users/nataliamalesa/Desktop/image-view
 const redis = require('redis');
 
 const app = express();
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-let client = redis.createClient();
-
-let redisMiddleware = (req, res, next) => {
-  let key = '__express__' + req.originalUrl || req.url;
-
-  client.get(key, (err, reply) => {
-    if (reply) {
-      res.send(reply);
-    } else {
-      res.sendResponse = res.send;
-      res.send = body => {
-        client.set(key, JSON.stringify(body));
-        res.sendResponse(body);
-      }
-      next();
-    }
-  });
-};
-
-app.get('/products/:id', redisMiddleware, async (req, res) => {
-  try {
-    let retrievedImage = await retrieveImage(req.params.id);
-    res.send(retrievedImage);
-  } catch (error) {
-    res.send(error);
-  }
+app.get('/', (req, res) => {
+  console.log('I just received a GET request on port 5000!');
+  res.send('Hello World');
 });
+
+// let client = redis.createClient();
+//
+// let redisMiddleware = (req, res, next) => {
+//   let key = '__express__' + req.originalUrl || req.url;
+//
+//   client.get(key, (err, reply) => {
+//     if (reply) {
+//       res.send(reply);
+//     } else {
+//       res.sendResponse = res.send;
+//       res.send = body => {
+//         client.set(key, JSON.stringify(body));
+//         res.sendResponse(body);
+//       }
+//       next();
+//     }
+//   });
+// };
+//
+// app.get('/products/:id', redisMiddleware, async (req, res) => {
+//   try {
+//     let retrievedImage = await retrieveImage(req.params.id);
+//     res.send(retrievedImage);
+//   } catch (error) {
+//     res.send(error);
+//   }
+// });
 
 app.listen(port, console.log(`Listening on port ${port}...`));
