1164e095f210215314584d8545d4c633e60bc0a7
Natalia Malesa <nmalesa@gmail.com>
2020-03-31 19:26:31 -0500

    Remove basic Express load balancer

--- index.js
+++ index.js
@@ -14,51 +14,33 @@ app.use(express.urlencoded({ extended: true }));
 //   port: 6379
 // });
 
-const app1 = express();
-const app2 = express();
+let client = redis.createClient();
 
-app1.use(express.json());
-app2.use(express.json());
+let redisMiddleware = (req, res, next) => {
+  let key = '__express__' + req.originalUrl || req.url;
 
-const handler = serverNum => (req, res) => {
-  console.log(`server ${serverNum}`, req.method, req.url, req.body);
-  res.send(`Hello from server ${serverNum}!`);
-}
+  client.get(key, (err, reply) => {
+    if (reply) {
+      res.send(reply);
+    } else {
+      res.sendResponse = res.send;
+      res.send = body => {
+        client.set(key, JSON.stringify(body));
+        res.sendResponse(body);
+      }
+      next();
+    }
+  });
+};
 
-app1.get('/', handler(1));
-app2.get('/', handler(2));
+app.get('/products/:id', redisMiddleware, async (req, res) => {
+  try {
+    let retrievedImage = await retrieveImage(req.params.id);
+    res.send(retrievedImage);
+    // console.log(`${process.env.MESSAGE}`);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
-app1.listen(8000);
-app2.listen(8001);
-
-
-// let client = redis.createClient();
-//
-// let redisMiddleware = (req, res, next) => {
-//   let key = '__express__' + req.originalUrl || req.url;
-//
-//   client.get(key, (err, reply) => {
-//     if (reply) {
-//       res.send(reply);
-//     } else {
-//       res.sendResponse = res.send;
-//       res.send = body => {
-//         client.set(key, JSON.stringify(body));
-//         res.sendResponse(body);
-//       }
-//       next();
-//     }
-//   });
-// };
-//
-// app.get('/products/:id', redisMiddleware, async (req, res) => {
-//   try {
-//     let retrievedImage = await retrieveImage(req.params.id);
-//     res.send(retrievedImage);
-//     console.log(`${process.env.MESSAGE}`);
-//   } catch (error) {
-//     res.send(error);
-//   }
-// });
-//
-// app.listen(port, console.log(`Listening on port ${port}...`));
+app.listen(port, console.log(`Listening on port ${port}...`));
