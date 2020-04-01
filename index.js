a40790f80691adb782d7c928f2732419723486f2
Natalia Malesa <nmalesa@gmail.com>
2020-03-31 19:50:37 -0500

    Add error handler for proxied request

--- index.js
+++ index.js
@@ -17,10 +17,14 @@ let cur = 0;
 
 // Proxy HTTP request with request library and pipe
 const handler = (req, res) => {
-  req.pipe(request({ url: servers[cur] + req.url })).pipe(res);
+  // Add an error handler for the proxied request
+  const _req = request({ url: servers[cur] + req.url }).on('error', error => {
+    res.status(500).send(error.message);
+  });
+  req.pipe(_req).pipe(res);
   cur = (cur + 1) % servers.length;
 };
-const server = express().get('*', handler).post('*', handler);
+const server = express().get('*', handler);
 
 server.listen(8080);
 
